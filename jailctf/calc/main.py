b971be8da38bd8f76a4831df56bfacb875377cb2
tomclima <tomecostalima1228@gmail.com>
2025-10-06 17:03:59 -0300

    jail: calc solution starting to make sense

---
+++ jailctf/calc/main.py
@@ -0,0 +1,53 @@
+#!/usr/local/bin/python3
+from sys import addaudithook
+from os import _exit
+from re import match
+
+
+def safe_eval(exit, code):
+    def hook(*a):
+        print("nono")
+        exit(0)
+    def disabled_exit(*a):
+        pass
+
+    def testing():
+        print("test")
+        return 1
+
+    def dummy():
+        pass
+
+    dummy.__code__ = compile(code, "<code>", "eval")
+    print("Activating audit hook...")
+    addaudithook(hook)
+    val = dummy()
+    
+    # audit hooks do not allow me to do important stuff afterwards, so i am disabling this one after eval completion
+    # surely this won't have unintended effects down the line, ... right?
+    print("Disabling audit hook...")
+    exit = disabled_exit
+
+    
+    payload = 2+(lambda:(print("a"),2)[1])()
+
+    print(payload)
+
+    return val
+
+
+
+if __name__ == "__main__":
+    expr = input("Math expression: ")
+  
+
+    if len(expr) <= 200 and match(r"[0-9+\-*/]+", expr):
+        # extra constraints just to make sure people don't use signal this time ...
+        if len(expr) <= 75 and ' ' not in expr and '_' not in expr:
+            print(safe_eval(_exit, expr))
+        else:
+            print('Unacceptable')
+    else:
+        print("Do you know what is a calculator?")
+    
+    print("reached")
